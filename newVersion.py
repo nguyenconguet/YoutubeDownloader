119fa3141e4724fbf1d87280d8a4aa0e011ae19e
TaiTuLangQuen <duycong.cn@gmail.com>
2021-09-27 14:59:08 +0700

    add title of video, change folder to save and make gui is easier

--- newVersion.py
+++ newVersion.py
@@ -1,5 +1,7 @@
 from tkinter import *
+from tkinter import filedialog
 from pytube import YouTube
+import os
 
 BG_COLOR = ""
 
@@ -17,13 +19,14 @@ class YoutubeDownloader:
         self.ls_yt_streams = None
         self.ls_streams_view = None
         self.map_view_items = dict()
+        self.path_folder = os.getcwd()
 
         # main frame
         self.main = main
         self.topFrame = Frame(main, bg="yellow")
         self.bottomFrame = Frame(main, bg="black")
         self.leftFrame = Frame(
-            self.bottomFrame, bg="green")
+            self.bottomFrame, bg="#ffff99")
         self.rightFrame = Frame(
             self.bottomFrame, bg="blue")
 
@@ -59,24 +62,34 @@ class YoutubeDownloader:
 
         # input
         self.inputLink = Entry(self.inputFrame, width=50,
-                               textvariable=StringVar(), font=('calibre', 15, 'normal'))
+                               textvariable=StringVar(), font=('Andale Mono', 14, 'normal'))
         self.inputLink.grid(row=0, column=0)
 
         # submit btn
         self.getBtn = Button(
-            self.inputFrame, text="Get Link", command=self.get_link)
+            self.inputFrame, text="Get Link", font=('Andale Mono', 12), command=self.get_link)
         self.getBtn.grid(row=0, column=1)
 
         # bottom frame
         # left frame
-        self.playwidget = ""
+        self.leftFrame.rowconfigure(0, weight=1)
+        self.leftFrame.rowconfigure(1, weight=1)
+        self.leftFrame.columnconfigure(0, weight=1)
+        self.youtubeTitle = Label(
+            self.leftFrame, wraplength=250, justify=CENTER, text="Title of video", font=('Andale Mono', 12), bg="#ffff99")
+        self.youtubeTitle.grid(row=0, column=0, sticky='nesw')
+
+        self.selectPathBtn = Button(
+            self.leftFrame, text="Select Save Folder", font=('Andale Mono', 12), bg="#cc9900", command=self.get_save_folder)
+        self.selectPathBtn.grid(
+            row=1, column=0, sticky='ew')
 
         # right frame
         self.rightFrame.rowconfigure(0, weight=1)
         self.rightFrame.rowconfigure(1, weight=3)
         self.rightFrame.columnconfigure(0, weight=1)
 
-        self.filterFrame = Frame(self.rightFrame, bg="black")
+        self.filterFrame = Frame(self.rightFrame)
         self.filterFrame.grid(row=0, column=0, sticky='nesw')
         self.filterFrame.rowconfigure(0, weight=1)
         self.filterFrame.columnconfigure(0, weight=1)
@@ -86,42 +99,46 @@ class YoutubeDownloader:
 
         self.radioOption = StringVar()
         self.video_audio_filter = Radiobutton(
-            self.filterFrame, text="Video & Audio", variable=self.radioOption, value="VideoAudio")
+            self.filterFrame, text="Video & Audio", font=('Andale Mono', 12), bg="#6699ff", variable=self.radioOption, value="VideoAudio")
         self.video_audio_filter.grid(row=0, column=0, sticky='news')
 
         self.video_filter = Radiobutton(
-            self.filterFrame, text="Video", variable=self.radioOption, value="Video")
+            self.filterFrame, text="Video", font=('Andale Mono', 12), bg="#6699ff", variable=self.radioOption, value="Video")
         self.video_filter.grid(row=0, column=1, sticky='news')
 
         self.audio_filter = Radiobutton(
-            self.filterFrame, text="Audio", variable=self.radioOption, value="Audio")
+            self.filterFrame, text="Audio", font=('Andale Mono', 12), bg="#6699ff", variable=self.radioOption, value="Audio")
         self.audio_filter.grid(row=0, column=2, sticky='news')
 
         self.filterBtn = Button(
-            self.filterFrame, text="Filter", command=self.get_filter)
+            self.filterFrame, text="Filter", font=('Andale Mono', 12), bg="#6699ff", command=self.get_filter)
         self.filterBtn.grid(row=0, column=3, sticky='news')
 
         self.rightBottomFrame = Frame(self.rightFrame, bg="red")
         self.rightBottomFrame.grid(row=1, column=0, sticky='news')
         self.rightBottomFrame.columnconfigure(0, weight=3)
         self.rightBottomFrame.columnconfigure(1, weight=1)
+        self.rightBottomFrame.rowconfigure(0, weight=1)
 
         # list view
-        self.listBox = Listbox(self.rightBottomFrame)
-        self.listBox.grid(row=0, column=0, sticky='news')
+        self.listBox = Listbox(self.rightBottomFrame, font=(
+            'Andale Mono', 12), bg="#ccff99")
+        self.listBox.grid(row=0, column=0, sticky='nsew')
 
         self.downloadBtn = Button(
-            self.rightBottomFrame, text='Download', command=self.download)
-        self.downloadBtn.grid(row=0, column=1)
+            self.rightBottomFrame, text='Download', font=('Andale Mono', 12), command=self.download)
+        self.downloadBtn.grid(row=0, column=1, sticky='ew')
 
     def get_link(self):
         url = str(self.inputLink.get())
         yt = YouTube(url)
         yt.register_on_progress_callback(self.on_progress)
 
-        # print(yt.streams)
+        print(yt.title)
+        self.youtubeTitle.config(text=yt.title)
+
         self.ls_yt_streams = yt.streams
-        print(self.ls_yt_streams)
+        # print(self.ls_yt_streams)
         # show to listBox
         self.listBox.delete(0, END)
         self.map_view_items.clear()
@@ -155,7 +172,7 @@ class YoutubeDownloader:
         if filterMethod == "VideoAudio":
             self.ls_streams_view = self.ls_yt_streams.filter(
                 progressive=True).order_by('resolution').desc()
-            print(self.ls_streams_view)
+            # print(self.ls_streams_view)
 
             self.listBox.delete(0, END)
             # self.map_view_items.clear()
@@ -168,7 +185,7 @@ class YoutubeDownloader:
         elif filterMethod == "Video":
             self.ls_streams_view = self.ls_yt_streams.filter(
                 progressive=False, type='video').order_by('resolution').desc()
-            print(self.ls_streams_view)
+            # print(self.ls_streams_view)
 
             self.listBox.delete(0, END)
             # self.map_view_items.clear()
@@ -181,7 +198,7 @@ class YoutubeDownloader:
         elif filterMethod == 'Audio':
             self.ls_streams_view = self.ls_yt_streams.filter(
                 progressive=False, type='audio').order_by('abr').desc()
-            print(self.ls_streams_view)
+            # print(self.ls_streams_view)
 
             self.listBox.delete(0, END)
             # self.map_view_items.clear()
@@ -192,14 +209,24 @@ class YoutubeDownloader:
                 self.map_view_items[item] = i
                 self.listBox.insert(END, item)
 
+    def get_save_folder(self):
+        self.path_folder = filedialog.askdirectory()
+
     def on_progress(self, stream, chunk, bytes_remaining):
         self.downloadBtn.configure(text="Downloading...", state=DISABLED)
-        print('DOWNLOADING...')
+        # print("downloading..")
+        filesize = round(stream.filesize / (1024 * 1024), 2)
+
+        download_size = round(
+            (stream.filesize - bytes_remaining) / (1024 * 1024), 2)
+
+        print('DOWNLOADING: ' + str(download_size) +
+              "MB/" + str(filesize) + "MB")
 
     def download(self):
         item = self.listBox.get(self.listBox.curselection())
         stream = self.map_view_items[item]
-        stream.download()
+        stream.download(self.path_folder)
         self.downloadBtn.configure(text="Download", state=NORMAL)
         print("Complete")
 
